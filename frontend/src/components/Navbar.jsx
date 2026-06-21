e6ae24d0c5e3b6d8f3ea3016ea0822e8a22e6752
Tanmay Haldar <iamtanmay2959@gmail.com>
2026-06-21 18:59:50 +0530

    production ready:

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,30 +26,31 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
     }
 
     return (
-        <div className="bg-white flex flex-col sm:flex-row items-center justify-between gap-3 px-4 sm:px-6 py-3 drop-shadow">
-            <h2 className="text-xl font-medium text-black py-2">
-                <Link to={"/"}>Notes</Link>
-            </h2>
+        <div className="bg-white flex flex-col px-4 sm:px-6 py-3 drop-shadow gap-3">
+            <div className="w-full flex items-center justify-between gap-3">
+                <h2 className="text-xl font-medium text-black py-2">
+                    <Link to="/">Notes</Link>
+                </h2>
 
-            <SearchBar
-                value={searchQuery}
-                onChange={({ target }) => {
-                    setSearchQuery(target.value);
-                }}
-                handleSearch={handleSearch}
-                onClearSearch={onClearSearch}
-            />
+                {userInfo ? (
+                    <ProfileInfo userInfo={userInfo} onLogOut={handleLogout} />
+                ) : (
+                    <button className="btn text-slate-400 rounded-md px-3 py-2">
+                        <Link className='hover:underline hover:text-blue-400' to="/login">Login</Link>
+                        /
+                        <Link className='hover:underline hover:text-blue-400' to="/signup"> SignUp</Link>
+                    </button>
+                )}
+            </div>
 
-            {userInfo ? (<ProfileInfo userInfo={userInfo} onLogOut={handleLogout} />) : (
-                <button className="btn text-slate-400 rounded-md px-3 py-2 ">
-                    <Link className='hover:underline hover:text-blue-400 ' to={"/login"}>Login </Link>
-                    /
-                    <Link className='hover:underline hover:text-blue-400 ' to={"/signup"}> SignUp</Link>
-                </button>
-            )}
-
-
-
+            <div className="w-full">
+                <SearchBar
+                    value={searchQuery}
+                    onChange={({ target }) => setSearchQuery(target.value)}
+                    handleSearch={handleSearch}
+                    onClearSearch={onClearSearch}
+                />
+            </div>
         </div>
     )
 }
